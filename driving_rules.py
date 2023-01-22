0cb655e45ec5a36d2a403f4a142627c005a5ba4e
Rohit Das <30646277+das-17@users.noreply.github.com>
2023-01-22 13:29:57 -0800

    Fall22-commit

--- driving_rules.py
+++ driving_rules.py
@@ -26,9 +26,12 @@ ANDS = [AND, THAT]
 
 OR = ' or '
 VERBS = ['VB', 'VBD', 'VBG', 'VBN', 'VBZ', 'VBP']
+# Sample keywords list to check and see if we're able to generate a triple based on that
+KEYWORDS = ['white stop line','crosswalk line']
 SUBJECTS = ['NN', 'NNP', 'NNS']
 
 TO_BE = ['is', 'am', 'are', 'was', 'were', 'be', 'being', 'been']
+SIGN_TYPES = ['white stop line','crosswalk',] # Add more stop signs prolly would need help identifying these
 TO_HAVE = ['have', 'has', 'had', 'having']
 NOTS = ['not', 'never']
 
@@ -52,9 +55,9 @@ def read_manual(state:str='MA', file_name='MA_Drivers_Manual.pdf', rule_file:str
 
     # printing number of pages in pdf file 
     #MAX_PAGES = pdfReader.numPages
-    MAX_PAGES = 2
+    MAX_PAGES = 20
     #    MAX_PAGES = 10
-    START_PAGE = 103 # This starts from the rules of the road for MA.# added change by RD start at 88
+    START_PAGE = 91 # This starts from the rules of the road for MA.# added change by RD start at 88-106(89)
     END_PAGE = START_PAGE+MAX_PAGES-1 #START_PAGE+40 # MAX_PAGES# testint page by page
     all_rules = []
     all_sentences = []
@@ -62,6 +65,7 @@ def read_manual(state:str='MA', file_name='MA_Drivers_Manual.pdf', rule_file:str
     """
     For Mass 82-124
     """
+    #Other options for triple format, why did we stick to triple format? is it utilized in a better way in the next part of the project?
     for page in range(START_PAGE, END_PAGE):
         pageObj = pdfReader.getPage(page)
         pageText = pageObj.extractText()
@@ -107,7 +111,9 @@ def extract_if_then(page_text: str):
        
         tokens = word_tokenize(sentence.lower())
         if IF_ in tokens and len(tokens) < MAX_WORDS:
-            print("Sentence:"+sentence+"\n")
+            print("Sentence: "+sentence+"\n")
+            if u'•' in sentence:
+                sentence = sentence.split(u'•')[1]
             words = [word for word in tokens if word.isalpha()]
             stripped = words[0]
             for item in words[1::]:
@@ -118,6 +124,7 @@ def extract_if_then(page_text: str):
                 logging.debug("Root it %s" % sentence.strip())
                 logging.debug("  Rule is:  %s" % rule)
                 counter += 1
+                print(rule) 
                 rules.append(rule)
                 all_sentences.append(stripped+"\n")
     return (rules, all_sentences)
@@ -172,31 +179,133 @@ def set_if_clause(clauses) -> Tuple:
         for item in clauses[1::]:
             full_then += item
         return clauses[0], full_then
-
-
-def make_triples_from_phrase(phrase: str, full_phrase: str = ""):
-    """
-    Struggled with this one. So I think we need to find all the occurences
-    Keeping a full phrase in case....
-    """
-    logging.debug("  Making triples for %s" % phrase)
+# Modified function with return type updates and previous triple storage
+pTriple = []
+def make_triples_from_phrase(phrase: str, full_phrase:str = ""):
+    logging.debug("  Making triples for %s"%phrase)
+    global pTriple
     if AND in phrase or OR in phrase or THAT in phrase:
-        print("and/or in phrase" + phrase)
         tokens = word_tokenize(phrase)
         for token in tokens:
             if token == AND.strip():
                 parts = phrase.split(AND, 1)
-                return "AND(%s, %s)" % (make_triples_from_phrase(parts[0]), make_triples_from_phrase(parts[1]))
+                triple1 = make_triples_from_phrase(parts[0])
+                if triple1 is not None:
+                 pTriple = triple1.strip('()').split(',')
+                triple2 = make_triples_from_phrase(parts[1])
+                #Make a keyword selection check here
+                #]keyword_selection(phrase,triple1)
+                #keyword_selection(phrase,triple2)
+                return "AND(%s, %s)" %(triple1, triple2)
             elif token == THAT.strip():
                 parts = phrase.split(THAT, 1)
-                return "AND(%s, %s)" % (make_triples_from_phrase(parts[0]), make_triples_from_phrase(parts[1]))
+                triple1 = make_triples_from_phrase(parts[0])
+                if triple1 is not None:
+                    pTriple = triple1.strip('()').split(',')
+                triple2 = make_triples_from_phrase(parts[1])
+                return "AND(%s, %s)" %(triple1, triple2)
             elif token == OR.strip():
                 parts = phrase.split(OR, 1)
-                return "OR(%s, %s)" % (make_triples_from_phrase(parts[0]), make_triples_from_phrase(parts[1]))
+                triple1 = make_triples_from_phrase(parts[0])
+                if triple1 is not None:
+                    pTriple = triple1.strip('()').split(',')
+                triple2 = make_triples_from_phrase(parts[1])
+                return "OR(%s, %s)" %(triple1, triple2)
+                
     else:
         return make_one_triple(phrase)
 
+def keyword_selection(phrase:str,obj: str):
+    """
+    Function to check if the keyword is split and formed in the triple. If so we replace it with the entire keyword
+    """   
+    for kword in KEYWORDS:
+        if kword in phrase:
+             return kword
+    return obj
+        
+    
+#def make_triples_from_phrase_original(phrase: str, full_phrase:str = ""):
+    """
+    Struggled with this one. So I think we need to find all the occurences
+    Keeping a full phrase in case....
+    """
+    # Original function with original functionality the above function is the v2.
+    # probably keep a global variable to keep the list of triples or keep a triple variable which has the previous variable but the issue here would be if there is 
+    # a  multiple split in the sentence then its hard to keep track
+    # we need to change the workflow for this function to adapt to the changes in make_one_triple
+    # How to split the work to independant study and MS project works and the timeline for MS might be extended.
+    # first order logic is used in the secondary system.
+    # probably store a set of keywords that represents lines, signs, boards, speed limits etc
+    # Few examples :
+    """ If there is a 
+       white stop line or crosswalk line, you must stop before the line
+       In this case rule not generated: what we need is
+       IF OR((self, isA, white stop line),(self,isA, crosswalk line)) THEN (stop, before, line)
+       currently it is (self, isA, stop) ln263 we need the list of keywords to be picked.
+       
+       Second case: If there are no lines, you 
+    must stop as close to the intersection as needed to see traffic in both directions
 
+    how should the triple be even formed?
+
+      Third case: If you are crossing an intersection, make sure 
+    you have enough room to make it completely through
+
+    Currently this is how it is formed: IF (self, isA, intersection), THEN (room, through, through)
+    
+    10/24/2022 updates: made a temporary list of keywords which swaps out in the triple when we have the wrong object picked up,
+    built a function called keyword_selection.
+    
+    10/31/2022 : Some tasks we could work on :- Task-1:find more keywords in sentence cases to see if rule makes sense or not
+    Task-2: get the multi-liason algo setup and working to check different triples versions for some of the sentences we have and see 
+    if existing triple is better or Mliason triple is better.
+    Task - 3: Find more triple cases that dont make sense.
+    #####################################################
+    
+    11/07/2022 : self refers to car? if so most triples sometimes are picked up wrong. 
+    Page 1 : If you are already stopped at an intersection or a stop 
+line, you may not proceed -> Not generating a triple.
+   >Page 1-2 : Sentence:  If you are turning left on a steady green light, you must yield to oncoming 
+traffic
+
+>IF (self, isA, light), THEN (self, yield, traffic)
+how to fix this?
+> AND seems to be working might need to make a separate function to prevent code redundancy.
+> Next task to focus on is making triples a lil better.
+ 
+    
+       
+    """
+ """   logging.debug("  Making triples for %s"%phrase)
+    if AND in phrase or OR in phrase or THAT in phrase:
+        tokens = word_tokenize(phrase)
+        for token in tokens:
+            if token == AND.strip():
+                parts = phrase.split(AND, 1)
+                triple1 = make_triples_from_phrase(parts[0])
+                if triple1 is not None:
+                 pTriple = triple1.strip('()').split(',')
+                triple2 = make_triples_from_phrase(parts[1])
+                #Make a keyword selection check here
+                #]keyword_selection(phrase,triple1)
+                #keyword_selection(phrase,triple2)
+                return "AND(%s, %s)" %(triple1, triple2)
+            elif token == THAT.strip():
+                parts = phrase.split(THAT, 1)
+                triple1 = make_triples_from_phrase(parts[0])
+                if triple1 is not None:
+                    pTriple = triple1.strip('()').split(',')
+                triple2 = make_triples_from_phrase(parts[1])
+                return "AND(%s, %s)" %(triple1, triple2)
+            elif token == OR.strip():
+                parts = phrase.split(OR, 1)
+                # "If there is a white stop line or crosswalk line" then triple is AND(self,isA,stop)(crosswalk,isA,None)
+                return "OR(%s, %s)" %(make_triples_from_phrase(parts[0]), make_triples_from_phrase(parts[1]))
+                
+    else:
+        return make_one_triple(phrase)
+"""
 def make_conjs(sentences):
     """
     Makes a conjunction from sentences.
@@ -249,18 +358,22 @@ def make_one_triple(sentence: str) -> str:
             return '(%s, %s, %s)' % (subject, relation, obj if object_phrase == "" else obj)
         # Otherwise can SVO or SPO (last NN->)
         elif has_verb(tags):  # Changed from truncated
-            verb = has_verb(tags)[0][0]
-            logging.debug("found verb %s" % verb)
+            # we need to track all the verbs and set up the rule in such a way that we pick the right verb maybe after the keyword? will need to check sentences based on sample set.
+            verb = has_verb(tags)[0][0]#Finding the first verb 
+            actionverb = tags[1][0] if tags[1][1] == "VBG" or "VBD" else ""
+            logging.debug("found verb %s"%verb)
             if verb_before_subject(tags):
                 obj = subject
-                subject = 'self'
+                subject = 'self'#check if verb exists before the subject and if so then make it self
             else:
+                
                 obj = get_object(truncated_tags[truncated_tags.index(has_verb(truncated_tags)[0])::])[0]
                 object_phrase = get_noun_phrase_if_exists(obj, sentence_cleaned)
                 obj = obj if object_phrase == "" else object_phrase
             if verb in TO_BE:
                 logging.debug("Found an isA type verb")
-                return '(%s, %s, %s)' % (subject, 'isA', obj)
+                obj = keyword_selection(sentence, obj)
+                return '(%s, %s, %s)' %(subject, 'isA', obj)
             elif verb in TO_HAVE:
                 logging.debug("Found an hasA type verb")
                 return '(%s, %s, %s)' % (subject, 'hasA', obj)
@@ -269,13 +382,24 @@ def make_one_triple(sentence: str) -> str:
         if neg:
             return 'NOT(%s, %s, %s)' % (subject, relation, obj)
         else:
-            return '(%s, %s, %s)' % (subject, relation, obj)
+            #if only_noun(truncated_tags):
+              #  return '(%s, %s, %s)' %(pTriple[0], pTriple[1], subject)
+            if obj == None:
+                keyword_selection(sentence, obj)
+                return '(%s, %s, %s)' %(pTriple[0], pTriple[1], subject)
+            else :
+             return '(%s, %s, %s)' %(subject, relation, obj)
     except TypeError:
         logging.debug("Could not make a triple for text %s" % sentence)
     except IndexError:
         logging.debug("Sentence: %s is blank" % sentence)
 
 
+def only_noun(tags):
+    for tag in tags:
+        if  tag[1] not in SUBJECTS:
+            return False
+    return True
 def has_in(tags):
     for tag in tags:
         if 'IN' == tag[1]:
@@ -323,6 +447,7 @@ def get_noun_phrase(tags):
 
 def get_noun_phrase_if_exists(start, sentence) -> str:
     for phrase in KEY_PHRASES:
+        # why are we checking start in phrase and then in sentence? start is already in sentence right
         if start in phrase and phrase in sentence:
             return phrase
     else:
